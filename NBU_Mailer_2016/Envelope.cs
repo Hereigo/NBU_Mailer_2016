e50db596d1bae6caf2eb3995fedfbc2f5aac7567
Hereigo <Hereigo@online.ua>
2016-02-03 17:23:49 +0200

    Add Start Settings & TodayEnvelopesDir Reader

---
+++ NBU_Mailer_2016/Envelope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NBU_Mailer_2016
+{
+    struct Envelope
+    {
+        public string envelopePath { get; set; }
+        public string envelopeName { get; set; }
+        public string sendFromAddress { get; set; }
+        public string recieveAddress { get; set; }
+        public string fileName { get; set; }
+        public int fileSize { get; set; }
+        public DateTime fileModified { get; set; }
+        public DateTime fileSent { get; set; }
+        public DateTime fileDelivered { get; set; }
+        public string fileLocation { get; set; }
+        public string fileCategory { get; set; }
+    }
+}
--- NBU_Mailer_2016/MainWindow.xaml.cs
+++ NBU_Mailer_2016/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,27 +24,92 @@ namespace NBU_Mailer_2016
     /// </summary>
     public partial class MainWindow : Window
     {
+        // SET ALL PATHES HERE :
+
+        // TODO: STATIC CLASS FOR STATIC FIELDS FOR SETTINGS
+
+        public static Logger nLogger = LogManager.GetCurrentClassLogger();
+
+        static string NbuRootDir;
+
+        static string envelopesTodayPath;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeParameters();
+
+            // TEMPORARY DEBUGGING !!!!!!!
+            // TEMPORARY DEBUGGING !!!!!!!
+            // TEMPORARY DEBUGGING !!!!!!!
+            foreach (var item in GetTodayEnvelopesList())
+                textBox_4_Tests_Only.Text += Environment.NewLine + item;
         }
 
 
-        private void InitStaticParams()
+        // - USEFUL CODE-SNIPPET :)  !!!!!!!!!!!!!!!
+        // - USEFUL CODE-SNIPPET :)  !!!!!!!!!!!!!!!
+        //
+        // string methodName = MethodInfo.GetCurrentMethod().Name;
+        // try{ }
+        // catch (Exception exc) { 
+        //     nLogger.Error(methodName + "() - " + exc.Message);
+        // }
+
+
+        // TODO: REFACTORE IT USING FOLDER-OPEN-DIALOG !!!
+        private void InitializeParameters()
         {
-            // 
+            string methodName = MethodInfo.GetCurrentMethod().Name;
+
+            string settsFile = "NBU_Mailer_2016.txt";
+            try
+            {
+                if (!File.Exists(settsFile))
+                {
+                    File.Create(settsFile);
+                    MessageBox.Show("Write Correct Full Path For NbuMail Files In First Line And Save It!");
+                    Process.Start(settsFile);
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    string readedString = File.ReadAllText(settsFile).Trim();
+                    if (Directory.Exists(readedString))
+                    {
+                        NbuRootDir = readedString;
+                        MessageBox.Show("Start Folder  ==  " + NbuRootDir);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Write Correct Full Path For NbuMail Files In First Line And Save It!");
+                        Process.Start(settsFile);
+                        Application.Current.Shutdown();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                nLogger.Error(methodName + "() - " + exc.Message);
+            }
         }
 
 
-        private void ReadTodayEnvelopesList()
+        private List<string> GetTodayEnvelopesList()
         {
-            
+            // SET TODAY ENVELOPES PATH EVERY RUN BECAUSE DEPENDS ON DATE !!!
 
-            // FOREACH - IF NOT EXISTS IN TODAY LOG-FILE
+            envelopesTodayPath = NbuRootDir + "ARH\\A" + DateTime.Now.ToString("ddMMyy") + "\\";
 
-            // ...
+            List<string> todayNewEnvelopes = new List<string>();
 
-            
+            foreach (FileInfo envelope in new DirectoryInfo(envelopesTodayPath).GetFiles())
+            {
+                todayNewEnvelopes.Add(envelope.Name);
+            }
+
+            return todayNewEnvelopes;
         }
 
 
